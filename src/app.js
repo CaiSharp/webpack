8f29fb0844a8119a81f837948a534b8916308bcb
Alex Weber <alex.weber312@gmail.com>
2018-10-29 09:46:20 +0100

    structure change

---
+++ src/app.js
@@ -0,0 +1,8 @@
+//IMPORT STYLES
+import './sass/main.scss'
+
+//IMPORT ASSETS
+import './img/avatar.jpg'
+
+//IMPORT SCRIPTS
+import './js/test'
--- src/js/app.js
+++
@@ -1,3 +0,0 @@
-import '../sass/main.scss'
-import '../img/avatar.jpg'
-console.log('test');
